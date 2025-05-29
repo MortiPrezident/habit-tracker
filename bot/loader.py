81baea2c4a1514140189ccb6427c64096aa46d2a
danila <danniila2000@mail.ru>
2025-05-29 16:30:31 +0300

    fix - исправлен некорректный адрес хоста

--- bot/loader.py
+++ bot/loader.py
@@ -5,7 +5,7 @@ from bot.scheduler_task.scheduler import BotScheduler
 from bot.config_data.config import DATABASE_URL
 
 
-redis_storage = StateRedisStorage(host="localhost", port=6379, db=0)
+redis_storage = StateRedisStorage(host="redis", port=6379, db=0)
 bot_tracker = AsyncTeleBot(token=BOT_TOKEN, state_storage=redis_storage)
 
 scheduler = BotScheduler(DATABASE_URL)
